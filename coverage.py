be251342f701e1d6fd512272ae05951e0001b83c
Lucas <micheltod@gmail.com>
2019-07-03 12:37:59 +0200

    Reestructured coverage.py. Added log to dividebyInput.

--- coverage.py
+++ coverage.py
@@ -1,22 +1,14 @@
 import os
 import subprocess as sp
 import pandas as pd
+import numpy as np
 
-indir = "/media/lucas/Disc4T/Projects/Cristina_ChIP_All/Coverage/Renamed/RPM/"
-wd = "/media/lucas/Disc4T/Projects/Cristina_ChIP_All/Coverage/Renamed/RPM/"
-os.chdir(wd)
-
-""" All files must be in the same directory. 
-All files must follow the naming convention: name_in/me/ac_... """
+# Functions
 
 
-bams = [b for b in os.listdir(indir) if b.endswith("q5.bam")]
-bams = [bams[0]]
-bams
+def removeDuplicates(bam):
+    """ Call GATK remove duplicates function on a bam file. """
 
-# 1. Remove Duplicates
-
-for bam in bams:
     i = indir+bam
     o = wd+bam.replace(".bam", "_noDup.bam")
     m = wd+bam.replace(".bam", "_metrics.txt")
@@ -26,39 +18,24 @@ for bam in bams:
 
     sp.call(cmd, shell=True)
 
-2. Index new bams
 
-bams = [b for b in os.listdir(wd) if b.endswith("_noDup.bam")]
-bams
-# for bam in bams:
-#     cmd = "samtools index {}" .format(bam)
-#     sp.call(cmd, shell=True)
+def indexBam(bam):
+    """ Index a bamfile using samtools. """
 
-# 3. Calculate number of reads
-
-nreads = {}
-for b in bams:
-
-    cmd = "samtools view -c {}" .format(b)
-    nreads[b] = int(sp.check_output(cmd, shell=True))
-
-# 4. Calculate coverage
+    cmd = "samtools index {}" .format(bam)
+    sp.call(cmd, shell=True)
 
 
 def coverage(bam, out):
+    """ Calculate per base coverage of a paired-end BAM file. """
+
     cmd = "bedtools genomecov -ibam {} -pc -d > {}" .format(bam, out)
     sp.call(cmd, shell=True)
 
 
-for bam in bams:
-    coverage(bam, bam.replace(".bam", "_depth.prebed"))
-
-# 5. Reformat depth file to proper BED
-
-prebeds = [b for b in os.listdir(wd) if b.endswith("_depth.prebed")]
-
-
 def properBED(prebed):
+    """ Reformat the output of bedtools genomecov to BED. """
+
     out = prebed.replace(".prebed", ".bdg")
     with open(out, "w+") as outfile:
         with open(prebed, "r+") as infile:
@@ -72,15 +49,10 @@ def properBED(prebed):
                 outfile.write("\t".join(bedline)+"\n")
 
 
-for pre in prebeds:
-    properBED(pre)
-
-# 6. Normalize by Number of Reads
-
-beds = [b for b in os.listdir(wd) if b.endswith("_depth.bdg")]
-
-
 def normalizeRPM(bed):
+    """ Divide coverage in each position by number of reads. 
+        Requires nreads, a dictinoray with bams as entries and 
+        number of reads as values. """
 
     with open(bed, "r+") as infile:
         with open(bed.replace(".bdg", "_RPM.bdg"), "w+") as outfile:
@@ -95,54 +67,31 @@ def normalizeRPM(bed):
                 outfile.write("\t".join(linelist[0:3])+"\t"+str(rpm)+"\n")
 
 
-for bed in beds:
-    normalizeRPM(bed)
+def divideByInput(chip_bed, input_bed, pseudo=0.1, log=True):
+    """ Divide chip_bed signal by chip_input signal.
+        Add a pseudocount to both chip and input.
+        Optionally log the result (log2). """
 
-# 7. Normalize by Input (RPM/RPM)
+    chip = pd.read_csv(chip_bed, sep="\t", header=None)
+    ctl = pd.read_csv(input_bed, sep="\t", header=None)
 
-rpm_beds = [b for b in os.listdir(wd) if b.endswith("_RPM.bdg")]
-rpm_beds
-# Make tupples met-in
-# Filenames must be in the form: name_in/me/ac_...
+    chip.iloc[:, 3] = (chip.iloc[:, 3]+pseudo)/(ctl.iloc[:, 3]+pseudo)
 
-# Divide by input
-pseudo = 0.1
+    if log:
+        chip.iloc[:, 3] = np.log2(chip.iloc[:, 3])
 
-for b in rpm_beds:
-    mark = b.split("_")[1]
-    prfx = b.split("_")[0]
-    if mark == "me" or mark == "ac":
-        treat_file = b
-        ctl_file = [x for x in rpm_beds
-                    if x.split("_")[0] == prfx and
-                    x.split("_")[1] == "in"][0]
-
-        treat = pd.read_csv(treat_file, sep="\t", header=None)
-        ctl = pd.read_csv(ctl_file, sep="\t", header=None)
-
-        treat.iloc[:, 3] = (treat.iloc[:, 3]+pseudo)/(ctl.iloc[:, 3]+pseudo)
-        treat.to_csv(path_or_buf=b.replace(
-            ".bdg", "_normIN.bdg"), sep="\t", index=False, header=None)
-
-# Substract Input
-for b in rpm_beds:
-    mark = b.split("_")[1]
-    prfx = b.split("_")[0]
-    if mark == "me" or mark == "ac":
-        treat_file = b
-        ctl_file = [x for x in rpm_beds
-                    if x.split("_")[0] == prfx and
-                    x.split("_")[1] == "in"][0]
-
-        treat = pd.read_csv(treat_file, sep="\t", header=None)
-        ctl = pd.read_csv(ctl_file, sep="\t", header=None)
-
-        treat.iloc[:, 3] = treat.iloc[:, 3]-ctl.iloc[:, 3]
-        treat.to_csv(path_or_buf=b.replace(
-            ".bdg", "_subsIN.bdg"), sep="\t", index=False, header=None)
+    chip.to_csv(path_or_buf=chip_bed.replace(
+        ".bdg", "_normIN.bdg"), sep="\t", index=False, header=None)
 
 
-# 8. Standarize Samples
+def substractInput(chip_bed, input_bed,):
+
+    treat = pd.read_csv(chip_bed, sep="\t", header=None)
+    ctl = pd.read_csv(input_bed, sep="\t", header=None)
+
+    treat.iloc[:, 3] = treat.iloc[:, 3]-ctl.iloc[:, 3]
+    treat.to_csv(path_or_buf=chip_bed.replace(
+        ".bdg", "_subsIN.bdg"), sep="\t", index=False, header=None)
 
 
 def standarizeBed(bed):
@@ -154,17 +103,8 @@ def standarizeBed(bed):
         ".bdg", "_STZ.bdg"), sep="\t", index=False, header=None)
 
 
-rpm_beds = [b for b in os.listdir(wd) if b.endswith("_subsIN.bdg")]
-
-for bed in rpm_beds:
-    standarizeBed(bed)
-
-# 9. Create IGV tracks for the bdg files.
-
-file_list = [b for b in os.listdir() if b.endswith(".bdg")]
-
-
 def calltoTDF(infile):
+    """ Input a .bdg file (bed) and create an IGV readable .tdf using igvtools. """
 
     outfile = infile+".tdf"
     cmd = ("~/Programs/IGV_2.4.10/IGVTools/igvtools toTDF "
@@ -173,6 +113,62 @@ def calltoTDF(infile):
 
     sp.call(cmd, shell=True)
 
+
+# Workflow
+# All files must be in the same directory.
+# All files must follow the naming convention: name_in/me/ac_...
+
+
+indir = "/media/lucas/Disc4T/Projects/Cristina_ChIP_All/Coverage/Renamed/RPM/"
+wd = "/media/lucas/Disc4T/Projects/Cristina_ChIP_All/Coverage/Renamed/RPM/"
+os.chdir(wd)
+
+bams = [b for b in os.listdir(indir) if b.endswith("q5.bam")]
+
+for bam in bams:
+    removeDuplicates(bam)
+
+bams = [b for b in os.listdir(wd) if b.endswith("_noDup.bam")]
+
+for bam in bams:
+    indexBam(bam)
+
+nreads = {}
+for b in bams:
+
+    cmd = "samtools view -c {}" .format(b)
+    nreads[b] = int(sp.check_output(cmd, shell=True))
+
+for bam in bams:
+    coverage(bam, bam.replace(".bam", "_depth.prebed"))
+
+prebeds = [b for b in os.listdir(wd) if b.endswith("_depth.prebed")]
+
+for pre in prebeds:
+    properBED(pre)
+
+beds = [b for b in os.listdir(wd) if b.endswith("_depth.bdg")]
+
+for bed in beds:
+    normalizeRPM(bed)
+
+rpm_beds = [b for b in os.listdir(wd) if b.endswith("_RPM.bdg")]
+
+for b in rpm_beds:
+    mark = b.split("_")[1]
+    prfx = b.split("_")[0]
+    if mark == "me" or mark == "ac":
+        treat_file = b
+        ctl_file = [x for x in rpm_beds
+                    if x.split("_")[0] == prfx and
+                    x.split("_")[1] == "in"][0]
+
+rpm_beds = [b for b in os.listdir(wd) if b.endswith("_subsIN.bdg")]
+
+for bed in rpm_beds:
+    standarizeBed(bed)
+
+file_list = [b for b in os.listdir() if b.endswith(".bdg")]
 
 for file in file_list:
     calltoTDF(file)
